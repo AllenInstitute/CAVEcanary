f990b9c764643f545a8d157757a89b32b1963762
Forrest Collman <forrest.collman@gmail.com>
2023-03-26 12:40:45 -0700

    adding health check

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+import threading
+from flask import Flask
 from canary import Canary
 
+app = Flask(__name__)
+canary_thread_exception = threading.Event()
 
-# main.py
-import os
-from pathlib import Path
 
-from canary import Canary
+def run_canary():
+    try:
+        canary = Canary()
+        canary.run()
+    except Exception as e:
+        print(f"Canary thread crashed with exception: {e}")
+        canary_thread_exception.set()
+
+
+@app.route("/health")
+def health_check():
+    if canary_thread_exception.is_set():
+        return "Canary thread crashed", 500
+    return "OK", 200
+
+
+def main():
+    canary_thread = threading.Thread(target=run_canary)
+    canary_thread.start()
+
+    app.run(host="0.0.0.0", port=80)
+
 
 if __name__ == "__main__":
-    canary = Canary()
-    canary.run()
+    main()

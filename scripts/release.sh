477e5bf89544b8f461f4f7c6e58c6e1b7274ff60
Forrest Collman <forrest.collman@gmail.com>
2023-03-27 07:53:32 -0700

    adding release script

---
+++ scripts/release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+bumpversion $1
+git push && git push --tags
+TAG=$(git describe --tags)
+docker build --platform linux/amd64 -t caveconnectome/cavecanary:${TAG} .
+docker push docker.io/caveconnectome/cavecanary:${TAG}
+# python setup.py sdist
+# twine upload dist/materializationengine-${TAG:1}.tar.gz

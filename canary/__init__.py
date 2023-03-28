e4f67a6b296f02e3bd1429c2eec5d8cda62852a0
Derrick Brittain <dlbrittain@gmail.com>
2023-03-28 10:53:38 -0700

    feat: fallback to old sample method if testing or not postgres

--- canary/__init__.py
+++ canary/__init__.py
@@ -4,6 +4,7 @@ import random
 import os
 import asyncio
 import configparser
+from urllib.parse import urlparse
 
 import numpy as np
 import pandas as pd
@@ -17,11 +18,10 @@ from slack_sdk.errors import SlackApiError
 
 class Canary:
     def __init__(self, client=None):
-
         self.config = configparser.ConfigParser()
         config_file = os.environ.get("CAVECANARY_CONFIG_FILE", "config.cfg")
         self.config.read(config_file)
-        self.database_uri = self.config["DATABASE_URI"]
+        self.database_uri = self.config.get("Settings", "DATABASE_URI")
         self.datastack_name = self.config.get("Settings", "DATASTACK_NAME")
         self.server_address = self.config.get(
             "Settings", "SERVER_ADDRESS", fallback=None
@@ -32,6 +32,7 @@ class Canary:
         self.num_test_annotations = self.config.getint(
             "Settings", "NUM_TEST_ANNOTATIONS"
         )
+        self.test_mode = self.config.getboolean("Settings", "TEST_MODE")
         if client is None:
             self.client = CAVEclient(
                 self.datastack_name,
@@ -49,31 +50,51 @@ class Canary:
             time.sleep(self.check_interval)
 
     async def check_random_annotations(self):
-        # create engine and connect to database
-        
         tables = self.client.materialize.get_tables()
-        
+
         async_engine = create_async_engine(self.database_uri)
         for table in tables:
             async with async_engine.begin() as conn:
                 # specify table name
-
-                # get a random sample of 1000 rows from the table
-                sample_query = f"SELECT * FROM {table} TABLESAMPLE BERNOULLI(10) LIMIT {self.num_test_annotations}"
                 try:
-                    result = await conn.execute(text(sample_query))
-                except Exception as e:
-                    self.send_slack_notification(f"Error in query_table: {e}")
+                    if self.test_mode or not self.is_postgres_url(self.database_uri):
+                        df = self.query_mat_table(table)
+                    else:
+                        # get a random sample of 1000 rows from the table
+                        sample_query = f"SELECT * FROM {table} TABLESAMPLE BERNOULLI(10) LIMIT {self.num_test_annotations}"
+                        result = await conn.execute(text(sample_query))
+
+                        # fetch the random sample of 1000 rows as a list of dictionaries
+                        data = [dict(row) async for row in result]
+                        df = pd.DataFrame(data)
+                except Exception as query_error:
+                    self.send_slack_notification(f"Error in query_table: {query_error}")
                     return
 
-                    # fetch the random sample of 1000 rows as a list of dictionaries
-                rows = [dict(row) async for row in result]
-                self.check_root_ids(pd.DataFrame(rows))
+                self.check_root_ids(df)
 
             # close database connection
             await async_engine.dispose()
 
-        
+    def query_mat_table(self, table):
+        num_rows = self.client.materialize.get_annotation_count(table)
+        # make the offset something that is not too close to the end
+        max_offset = max(num_rows - self.num_test_annotations, 0)
+        offset = random.randint(0, max_offset)
+
+        if num_rows < 100000:
+            df = self.client.materialize.query_table(
+                table, offset=offset, limit=self.num_test_annotations
+            )
+        else:
+            df = self.client.materialize.query_table(
+                table,
+                filter_in_dict={
+                    "id": np.arange(offset, offset + self.num_test_annotations)
+                },
+            )
+        return df
+
     def check_root_ids(self, df):
         supervoxel_columns = [
             col for col in df.columns if col.endswith("_supervoxel_id")
@@ -106,7 +127,10 @@ class Canary:
 
     def send_slack_notification(self, message):
         try:
-            self.slack_client.chat_postMessage(
-                channel=self.slack_channel, text=message)
+            self.slack_client.chat_postMessage(channel=self.slack_channel, text=message)
         except SlackApiError as e:
             print(f"Error sending Slack message: {e}")
+
+    def is_postgres_url(self, url):
+        parsed_url = urlparse(url)
+        return parsed_url.scheme.startswith("postgres")

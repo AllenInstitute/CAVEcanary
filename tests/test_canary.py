01f945680c517e022b552360b329477aa39bc937
Derrick Brittain <dlbrittain@gmail.com>
2023-03-28 10:53:06 -0700

    test: use pytest to use async

--- tests/test_canary.py
+++ tests/test_canary.py
@@ -1,18 +1,22 @@
-import unittest
-from unittest.mock import MagicMock, patch
-import pandas as pd
-import numpy as np
-from canary import Canary
+import asyncio
 import os
+
+import numpy as np
+import pandas as pd
+import pytest
+from canary import Canary
+from caveclient import CAVEclient
+from slack_sdk import WebClient
+from unittest.mock import patch
 
 os.environ["CAVECANARY_CONFIG_FILE"] = "tests/test_config.cfg"
 
 
-class TestCanary(unittest.TestCase):
-    @patch("caveclient.CAVEclient")
-    @patch("slack_sdk.WebClient")
-    def setUp(self, mock_slack_client, mock_cave_client):
-        
+@pytest.fixture
+def mock_canary():
+    with patch("caveclient.CAVEclient") as mock_cave_client, patch(
+        "slack_sdk.WebClient"
+    ) as mock_slack_client:
         # Mock client.info.get_datastack_info with test values
         mock_datastack_info = {
             "soma_table": "test_nucleus_neuron_svm",
@@ -34,64 +38,67 @@ class TestCanary(unittest.TestCase):
         }
 
         mock_cave_client.info.get_datastack_info.return_value = mock_datastack_info
-        self.canary = Canary(client=mock_cave_client)
-        self.canary.client = mock_cave_client
-        self.canary.slack_client = mock_slack_client
+        canary = Canary(client=mock_cave_client)
+        canary.client = mock_cave_client
+        canary.slack_client = mock_slack_client
 
-    def test_matching_root_ids(self):
-        # Mock table metadata and DataFrame with matching root IDs
-        self.canary.client.materialize.get_tables.return_value = ["test_table"]
-        self.canary.client.materialize.get_annotation_count.return_value = 10
-        data = {
-            "pre_supervoxel_id": [1, 2, 3],
-            "pre_rootid": [11, 22, 33],
-            "post_supervoxel_id": [4, 5, 6],
-            "post_rootid": [44, 55, 66],
-        }
-        df = pd.DataFrame(data)
-        self.canary.client.materialize.query_table.return_value = df
-        self.canary.client.chunkedgraph.get_roots.side_effect = (
-            lambda x, **kwargs: np.array([11, 22, 33])
-            if x[0] == 1
-            else np.array([44, 55, 66])
-        )
-
-        self.canary.check_random_annotations()
-
-        self.canary.slack_client.chat_postMessage.assert_not_called()
-
-    def test_non_matching_root_ids(self):
-        # Mock table metadata and DataFrame with non-matching root IDs
-        self.canary.client.materialize.get_tables.return_value = ["test_table"]
-        self.canary.client.materialize.get_annotation_count.return_value = 10
-        data = {
-            "pre_supervoxel_id": [1, 2, 3],
-            "pre_rootid": [11, 22, 33],
-            "post_supervoxel_id": [4, 5, 6],
-            "post_rootid": [44, 55, 99],  # Non-matching root ID
-        }
-        df = pd.DataFrame(data)
-        self.canary.client.materialize.query_table.return_value = df
-        self.canary.client.chunkedgraph.get_roots.side_effect = (
-            lambda x, **kwargs: np.array([11, 22, 33])
-            if x[0] == 1
-            else np.array([44, 55, 66])
-        )
-
-        self.canary.check_random_annotations()
-
-        self.canary.slack_client.chat_postMessage.assert_called_once()
-
-    def test_http_error(self):
-        # Mock table metadata and simulate HTTP error
-        self.canary.client.materialize.get_tables.return_value = ["test_table"]
-        self.canary.client.materialize.get_annotation_count.return_value = 10
-        self.canary.client.materialize.query_table.side_effect = Exception("HTTP Error")
-
-        self.canary.check_random_annotations()
-
-        self.canary.slack_client.chat_postMessage.assert_called_once()
+        yield canary
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.asyncio
+async def test_matching_root_ids(mock_canary):
+    # Mock table metadata and DataFrame with matching root IDs
+    mock_canary.client.materialize.get_tables.return_value = ["test_table"]
+    mock_canary.client.materialize.get_annotation_count.return_value = 10
+    data = {
+        "pre_supervoxel_id": [1, 2, 3],
+        "pre_rootid": [11, 22, 33],
+        "post_supervoxel_id": [4, 5, 6],
+        "post_rootid": [44, 55, 66],
+    }
+    df = pd.DataFrame(data)
+    mock_canary.client.materialize.query_table.return_value = df
+    mock_canary.client.chunkedgraph.get_roots.side_effect = (
+        lambda x, **kwargs: np.array([11, 22, 33])
+        if x[0] == 1
+        else np.array([44, 55, 66])
+    )
+
+    await mock_canary.check_random_annotations()
+
+    mock_canary.slack_client.chat_postMessage.assert_not_called()
+
+
+@pytest.mark.asyncio
+async def test_non_matching_root_ids(mock_canary):
+    # Mock table metadata and DataFrame with non-matching root IDs
+    mock_canary.client.materialize.get_tables.return_value = ["test_table"]
+    mock_canary.client.materialize.get_annotation_count.return_value = 10
+    data = {
+        "pre_supervoxel_id": [1, 2, 3],
+        "pre_rootid": [11, 22, 33],
+        "post_supervoxel_id": [4, 5, 6],
+        "post_rootid": [44, 55, 99],  # Non-matching root ID
+    }
+    df = pd.DataFrame(data)
+    mock_canary.client.materialize.query_table.return_value = df
+    mock_canary.client.chunkedgraph.get_roots.side_effect = (
+        lambda x, **kwargs: np.array([11, 22, 33])
+        if x[0] == 1
+        else np.array([44, 55, 66])
+    )
+    await mock_canary.check_random_annotations()
+
+    mock_canary.slack_client.chat_postMessage.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_http_error(mock_canary):
+    # Mock table metadata and simulate HTTP error
+    mock_canary.client.materialize.get_tables.return_value = ["test_table"]
+    mock_canary.client.materialize.get_annotation_count.return_value = 10
+    mock_canary.client.materialize.query_table.side_effect = Exception("HTTP Error")
+
+    await mock_canary.check_random_annotations()
+
+    mock_canary.slack_client.chat_postMessage.assert_called_once()

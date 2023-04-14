dbbac1ab34d2a7361257a2a4ef9c6ef2c74094e9
Derrick Brittain <dlbrittain@gmail.com>
2023-04-14 16:06:16 -0700

    feat: send error msg to logging

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import threading
 import asyncio
+import logging
 
 from flask import Flask
 from canary import Canary
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 canary_thread_exception = threading.Event()
@@ -10,13 +13,14 @@ canary_thread_exception = threading.Event()
 
 def run_canary():
     try:
+        logging.info("Starting...")
         canary = Canary()
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         asyncio.get_event_loop().run_until_complete(canary.run())
     except Exception as e:
         canary.send_slack_notification(f"Canary thread crashed with exception: {e}")
-        print(f"Canary thread crashed with exception: {e}")
+        logging.error(f"Canary thread crashed with exception: {e}")
         canary_thread_exception.set()
 
 

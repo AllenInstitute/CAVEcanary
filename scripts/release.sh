51111d9eb88392a88fc57f205653ebaddab10cae
Forrest Collman <forrest.collman@gmail.com>
2023-04-17 12:51:42 -0700

    fixing deployment examples

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 bumpversion $1
 git push && git push --tags
 TAG=$(git describe --tags)
-docker build --platform linux/amd64 -t caveconnectome/cavecanary:${TAG} .
-docker push docker.io/caveconnectome/cavecanary:${TAG}
+#docker build --platform linux/amd64 -t caveconnectome/cavecanary:${TAG} .
+#docker push docker.io/caveconnectome/cavecanary:${TAG}
 # python setup.py sdist
 # twine upload dist/materializationengine-${TAG:1}.tar.gz
